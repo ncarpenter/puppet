514e9dca80734a66c30b3e1ff92fad550bf347c6
ncarpenter <ncarpenter@me.com>
2015-01-18 21:02:22 -0700

    key

--- modules/user/manifests/adminusers.pp
+++ modules/user/manifests/adminusers.pp
@@ -12,4 +12,9 @@ home => '/home/neeko',
 managehome => 'true',
 password => 'neeko',
 }
+ssh_authorized_key { 'neeko_key':
+user => 'neeko',
+type => 'rsa',
+key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdAgEQFXGfV13kEKheh+GcBaXFsZUZlYQIfDnrNuq6WxUmxx+ucotc2SU3wj4yfvC1KeD47fWR65Jbf/MBZNzrlZg8PmcvX8YnR0GUOGA4igy4XIATuU3XGrIGwJ7GiH8wFHnAK+yJOvbq4YStMd/o619tbhhQhEF0HtTqPFdre4BTB4N3pHyuvART9iGJZIBd3S1MZtr6irJsPTKGe/6u/NlAlShOemXvuzmYVt/HCSjtI7/k21KgSEQwker1YTGXzuh6GXxrzW+7aJdzutifnhLxyAY+TMLRb08cnvPzrAH7wFNX/nVgJpu+59WPW6ytONmV5mRqpa43nrXONZq3'
+}
 }
