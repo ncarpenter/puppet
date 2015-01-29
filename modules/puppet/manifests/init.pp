bf43614e33b417af7c64a87e6743e1d7dc1fc0a3
ncarpenter <ncarpenter@me.com>
2015-01-29 15:22:30 -0700

    t

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -14,8 +14,8 @@ class puppet {
  file {'/etc/puppet/hiera':
    ensure => directory,
  }
- file {'/etc/puppet/hiera.yaml':
-   source  => 'puppet:///modules/puppet/hiera/hiera.yaml',
+ file {'/etc/puppet/hiera/hiera.yaml':
+   source  => 'puppet:///modules/puppet/hiera.yaml',
    require => File['/etc/puppet/hiera'],
  }
 
